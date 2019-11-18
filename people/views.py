f68d003e7a4f6173f6f18eddd8c2d893cbeb8ffd
Jose Jonatas <josejonatasoliveira.jjo@gmail.com>
2019-11-18 20:50:09 -0200

    ajustes no profile

--- people/views.py
+++ people/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
